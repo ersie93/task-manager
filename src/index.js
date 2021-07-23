388d9dbc1f3dc7277c3246e69294182127b38f5c
ryan erswell <ersie93@googlemail.com>
2021-07-23 15:22:16 +0100

    added authorization to task routes

--- src/index.js
+++ src/index.js
@@ -14,16 +14,16 @@ app.listen(port, ()=>{
 	console.log("listening on port " + port)
 })
 
-const User = require('./models/user')
+// const User = require('./models/user')
 
-const main = async () => {
-	// const task = await Task.findById('60faaef0d0c8cc61efe63f2a')
-	// await task.populate('owner').execPopulate()
-	// console.log(task.owner)
+// const main = async () => {
+// 	// const task = await Task.findById('60faaef0d0c8cc61efe63f2a')
+// 	// await task.populate('owner').execPopulate()
+// 	// console.log(task.owner)
 
-	const user = await User.findById("60faaeced0c8cc61efe63f21")
-	await user.populate('tasks').execPopulate()
-	console.log(user.tasks)
-}
+// 	const user = await User.findById("60faaeced0c8cc61efe63f21")
+// 	await user.populate('tasks').execPopulate()
+// 	console.log(user.tasks)
+// }
 
-main()
+// main()
