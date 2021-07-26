a9bc8de98e8d841ed2ef050b5aa9ec917440dd8b
ryan erswell <ersie93@googlemail.com>
2021-07-26 11:53:40 +0100

    added file upload using multer

--- src/index.js
+++ src/index.js
@@ -6,6 +6,17 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+
+// const multer = require('multer')
+// const upload = multer({
+// 	dest: 'images'
+// })
+
+// app.post('/upload', upload.single('upload'), (req, res) => {
+// 	res.send()
+// })
+
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -13,17 +24,3 @@ app.use(taskRouter)
 app.listen(port, ()=>{
 	console.log("listening on port " + port)
 })
-
-// const User = require('./models/user')
-
-// const main = async () => {
-// 	// const task = await Task.findById('60faaef0d0c8cc61efe63f2a')
-// 	// await task.populate('owner').execPopulate()
-// 	// console.log(task.owner)
-
-// 	const user = await User.findById("60faaeced0c8cc61efe63f21")
-// 	await user.populate('tasks').execPopulate()
-// 	console.log(user.tasks)
-// }
-
-// main()
