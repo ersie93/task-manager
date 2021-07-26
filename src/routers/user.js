208af3dc0c2901720153b2d2dacd673dfeeca8f5
ryan erswell <ersie93@googlemail.com>
2021-07-26 15:57:54 +0100

    added get route for avatar and crop images and save as png

--- src/routers/user.js
+++ src/routers/user.js
@@ -3,6 +3,7 @@ const router = new express.Router()
 const User = require('../models/user')
 const auth = require('../middleware/auth')
 const multer = require('multer')
+const sharp = require('sharp')
 
 router.post('/users', async (req, res) => {
 	const user = new User(req.body)
@@ -89,7 +90,8 @@ const upload = multer({
 })
 
 router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
-	req.user.avatar = req.file.buffer
+	const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
+	req.user.avatar = buffer
 	await req.user.save()
 	res.send()
 }, (error, req, res, next) => {
@@ -100,6 +102,19 @@ router.delete('/users/me/avatar', auth, async (req, res) => {
 	req.user.avatar = undefined
 	await req.user.save()
 	res.send()
+})
+
+router.get('/users/:id/avatar', async (req, res) => {
+	try {
+		const user = await User.findById(req.params.id)
+		if(!user || !user.avatar){
+			throw new Error()
+		}
+		res.set('Content-Type', 'image/png')
+		res.send(user.avatar)
+	} catch(e) {
+		res.status(400).send()
+	}
 })
 
 
